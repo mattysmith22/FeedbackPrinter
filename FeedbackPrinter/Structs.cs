f58d034d6fea8c183772c2a953b019816e3b0901
matthew <mattysmith22@googlemail.com>
2017-12-21 17:54:08 +0000

    Add project files.

---
+++ FeedbackPrinter/Structs.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace FeedbackPrinter
+{
+    struct FeedBackData
+    {
+        public string studentName; //The name of the student
+        public string topicName;
+        public DateTime date;
+        public string teacherName;
+        public string topicNote;
+        public string targetGrade;
+        public string acheivedGrade;
+        public string effortGrade;
+        public string[] topicsCovered;
+        public string[] feedbackWWW;
+        public string[] feedbackTIF;
+
+    }
+
+    struct Fonts
+    {
+        public Font titles;
+        public Font normal;
+        public Font title;
+        public Font feedback;
+    }
+}
